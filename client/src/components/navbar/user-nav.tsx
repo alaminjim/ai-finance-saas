ecc32265f842dd68740847fcf3728c07f9793630
Al-Amin Islam <jimalamin7@gmail.com>
2026-05-07 22:06:55 +0600

    fix navbar

--- client/src/components/navbar/user-nav.tsx
+++ client/src/components/navbar/user-nav.tsx
@@ -8,11 +8,10 @@ import {
   import {
     DropdownMenu,
     DropdownMenuContent,
-    DropdownMenuGroup,
     DropdownMenuItem,
     DropdownMenuLabel,
     DropdownMenuSeparator,
-        DropdownMenuTrigger,
+    DropdownMenuTrigger,
   } from "../ui/dropdown-menu"
   
 export function UserNav({
@@ -29,42 +28,37 @@ export function UserNav({
         <DropdownMenuTrigger asChild>
           <Button
             variant="ghost"
-            className="relative !bg-transparent h-8 w-8 rounded-full !gap-0"
+            className="relative bg-transparent h-10 w-10 rounded-full p-0 hover:bg-white/10"
           >
-            <Avatar className="h-10 w-10 !cursor-pointer ">
+            <Avatar className="h-8 w-8 cursor-pointer">
               <AvatarImage
                 src={profilePicture || ""}
-                className="!cursor-pointer "
+                className="object-cover"
               />
-              <AvatarFallback
-                className="!bg-[var(--secondary-dark-color)] border !border-gray-700
-                 !text-white"
-              >
+              <AvatarFallback className="bg-gray-700 text-white border border-gray-600">
                 {userName.charAt(0).toUpperCase()}
               </AvatarFallback>
             </Avatar>
-            <ChevronDown className="!w-3 !h-3 ml-1 text-white" />
+            <ChevronDown className="w-3 h-3 ml-1 text-white" />
           </Button>
         </DropdownMenuTrigger>
-      <DropdownMenuContent
-        className="w-56 !bg-[var(--secondary-dark-color)] !text-white
-         !border-gray-700 !z-50
-        "
-        align="end"
-      >
-        <DropdownMenuLabel className="flex flex-col items-start gap-1">
-          <span className="font-semibold">{userName}</span>
+        <DropdownMenuContent
+          className="w-56 bg-gray-800 text-white border-gray-700 z-[9999]"
+          align="end"
+          sideOffset={5}
+        >
+          <DropdownMenuLabel className="flex flex-col items-start gap-1">
+            <span className="font-semibold">{userName}</span>
             <span className="text-[13px] text-gray-400 font-light">Free Trial (2 days left)</span>
-           </DropdownMenuLabel>
-           <DropdownMenuSeparator className="!bg-gray-700" />
-           <DropdownMenuGroup>
-          <DropdownMenuItem className="hover:!bg-gray-800 hover:!text-white"
-          onClick={onLogout}
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator className="bg-gray-700" />
+          <DropdownMenuItem 
+            className="hover:bg-gray-700 hover:text-white cursor-pointer"
+            onClick={onLogout}
           >
             <LogOut className="w-4 h-4 mr-2" />
             Log out
           </DropdownMenuItem>
-          </DropdownMenuGroup>
         </DropdownMenuContent>
       </DropdownMenu>
     )
